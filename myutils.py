fb0b86fb7e9bb91ee2f0a26580bfd0ba83bee5c9
Alessandro Marzo <alessandro.marzo@orbitaleye.nl>
2023-11-05 01:19:19 +0100

    add game script

--- myutils.py
+++ myutils.py
@@ -1,4 +1,48 @@
+import cv2
+import numpy as np
+from PIL import Image
 
+def get_reference_corners():
+    perfect_corners = []
+    for i in range(1,8):
+        for j in range(1,8):
+            bbox = [60*i, 60*j]
+            perfect_corners.append(bbox)
+    
+    corners_ref = np.array(perfect_corners)
+
+    return corners_ref
+
+
+def calibrate_image(image_test_rgb, corners_ref, shape_ref, height, width):
+    image_test_gray = cv2.cvtColor(image_test_rgb, cv2.COLOR_RGB2GRAY)
+    
+    ret_test, corners_test = cv2.findChessboardCornersSB(image_test_gray, (7, 7), cv2.CALIB_CB_EXHAUSTIVE)
+    
+    if ret_test:
+        homography, mask = cv2.findHomography(corners_test, corners_ref, cv2.RANSAC)
+        # homography, mask = cv2.findHomography(corners_ref, corners_test, cv2.RANSAC)
+  
+        transformed_img = cv2.warpPerspective(image_test_rgb, homography, (width, height))
+        
+        output_image = transformed_img[:shape_ref[0],:shape_ref[1]]
+        
+    else:
+        output_image = image_test
+        output_image = cv2.resize(output_image, shape_ref, interpolation = cv2.INTER_LINEAR)
+ 
+    return ret_test, output_image
+
+
+def predict_yolo(image, model, shape_ref):
+    yolo_image = Image.fromarray(image)
+    results = model(yolo_image)
+
+    yolo_bboxes = results.xywhn[0].numpy()
+    predictions_bboxes = [bbox_yolo_to_pascal_voc(yolo_bbox[0], yolo_bbox[1], yolo_bbox[2], yolo_bbox[3], yolo_bbox[5], shape_ref[0], shape_ref[1]) for yolo_bbox in yolo_bboxes if yolo_bbox[4] > 0.75]
+    centers = [[np.floor(yolo_bbox[0]*8) + 1, np.floor(yolo_bbox[1]*8) + 1] for yolo_bbox in yolo_bboxes if yolo_bbox[4] > 0.75]
+    
+    return predictions_bboxes, centers
 
 
 def fen_to_board(fen):
@@ -23,6 +67,7 @@ def fen_to_board(fen):
         
     return board
 
+
 def bboxes_from_chess_board(chess_board_position):
     bboxes = []
     for i in range(8):
@@ -39,9 +84,11 @@ def bboxes_from_chess_board(chess_board_position):
                 # bboxes.append(bbox)
     return bboxes
 
+
 def bbox_to_yolobbox(bbox, image_w, image_h):
     class_id, x1, y1, x2, y2 = bbox
     return [class_id, ((x2 + x1)/(2*image_w)), ((y2 + y1)/(2*image_h)), (x2 - x1)/image_w, (y2 - y1)/image_h]
+
 
 def bbox_yolo_to_pascal_voc(x_center, y_center, w, h, class_id, image_w, image_h):
     w = w * image_w
--- run_game_chess_board.py
+++ run_game_chess_board.py
@@ -1,14 +1,21 @@
+import io
 import cv2
 import time
-import numpy as np
-# from skimage.transform import resize
 import torch
+import numpy as np
 from PIL import Image
+
+import chess
+import chess.svg
+import cairosvg
 
 from picamera2 import Picamera2
 
 from myutils import get_reference_corners, calibrate_image, predict_yolo
-        
+
+board = chess.Board()
+x_chess_board = 'abcdefgh'
+
 corners_ref = get_reference_corners()
 
 shape_ref = [480, 480] 
@@ -26,6 +33,8 @@ new_frame_time = 0
 
 # out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'MJPG'), 1.0, (480,480))
 
+old_centers = None
+
 while True:
     new_frame_time = time.time() 
     img = picam2.capture_array()
@@ -40,19 +49,29 @@ while True:
     ret, output_image = calibrate_image(image_test_rgb, corners_ref, shape_ref, height, width)
     
     if ret:
-        predictions_bboxes, new_centers = predict_yolo(output_image, model, shape_ref)   
-    
-        for bbox in predictions_bboxes:
-            if bbox[4] == 0:
-                color = (255, 0, 0)
+        predictions_bboxes, new_centers = predict_yolo(output_image, model, shape_ref)  
+        
+        if old_centers is None:
+            old_centers = new_centers 
+        
+        new_pos = [center for center in new_centers if center not in old_centers]
+        
+        move = ''
+        if new_pos is not None:
+            old_pos = [center for center in old_centers if center not in new_centers]
+
+            for i in range(len(new_pos)):
+                move += x_chess_board[int(old_pos[i][0]) - 1] + str(int(old_pos[i][1]))
+                move += x_chess_board[int(new_pos[i][0]) - 1] + str(int(new_pos[i][1]))
+        
+            if chess.Move.from_uci(move) in board.legal_moves:
+                board.push_uci(move)
             else:
-                color = (0, 255, 0)
-        
-            output_image = cv2.rectangle(output_image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
-        
-        output_image = cv2.cvtColor(output_image, cv2.COLOR_RGB2BGR)
-        
-        
+                print(move)
+                
+        lastmove = chess.Move.from_uci(move) if move else move
+        output_image = Image.open(io.BytesIO(cairosvg.svg2png(chess.svg.board(board, lastmove=lastmove, size=480))))
+                
 
     fps = 1/(new_frame_time-prev_frame_time) 
     prev_frame_time = new_frame_time 
